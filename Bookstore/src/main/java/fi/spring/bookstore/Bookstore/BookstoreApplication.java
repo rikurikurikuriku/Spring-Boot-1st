26e3a1dc79abcb99c656d48e1a3fb4ead261171f
Riku <71032160+rikurikurikuriku@users.noreply.github.com>
2021-09-12 12:14:51 +0300

    New version of Bookstore / week 36

--- Bookstore/src/main/java/fi/spring/bookstore/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/fi/spring/bookstore/Bookstore/BookstoreApplication.java
@@ -1,7 +1,15 @@
 package fi.spring.bookstore.Bookstore;
 
+import org.springframework.boot. CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import fi.spring.bookstore.Bookstore.domain.Book;
+import fi.spring.bookstore.Bookstore.domain.BookRepository;
+import fi.spring.bookstore.Bookstore.domain.Category;
+import fi.spring.bookstore.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -9,5 +17,16 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner demo(CategoryRepository repository1, BookRepository repository) {
+	return (args) -> {
+		
+		repository1.save(new Category("Fiction"));
+		repository1.save(new Category("Non-Fiction"));
+		
+		repository.save(new Book(1, "The Old Man and The Sea", "Ernest Hemingway", 1934, 1111, 19.90, repository1.findByName("Fiction").get(0)));	
+	 
+	};
+	}
+	
 }
---
+++ Bookstore/src/main/java/fi/spring/bookstore/Bookstore/web/BookController.java
@@ -0,0 +1,73 @@
+package fi.spring.bookstore.Bookstore.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.spring.bookstore.Bookstore.domain.Book;
+import fi.spring.bookstore.Bookstore.domain.BookRepository;
+import fi.spring.bookstore.Bookstore.domain.CategoryRepository;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookRepository bookspository;
+	
+	@Autowired
+	private CategoryRepository crepository;
+
+	@RequestMapping("/books")
+	public String allBooks(Model model) {
+		
+		model.addAttribute("books", bookspository.findAll());
+		return "books";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		model.addAttribute("categories", crepository.findAll());
+		return "addbook";
+	}
+	
+	@GetMapping(value = "/edit/{id}")
+	public String edit(@PathVariable("id") long id, Model model) {
+	    Book book = bookspository.findById(id)
+	      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	    
+	    model.addAttribute("book", book);
+	    return "edit";
+	}
+	
+	@PostMapping(value = "/update/{id}")
+	public String updateUser(@PathVariable("id") long id, @Valid Book book, 
+	  BindingResult result, Model model) {
+	    if (result.hasErrors()) {
+	        book.setId(id);
+	        return "edit";
+	    }
+	    bookspository.save(book);
+	    return "redirect:/books";
+	    
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		bookspository.save(book);
+		return "redirect:books";
+	}
+	
+	 @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	    public String deleteBook(@PathVariable("id") Long id, Model model) {
+	    	bookspository.deleteById(id);
+	        return "redirect:../books";
+	    }     
+}
